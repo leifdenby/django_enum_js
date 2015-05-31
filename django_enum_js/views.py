bda88dfe6e0a2f16f0c3be74a42cf8783aae1d9e
Leif Denby <leif@denby.eu>
2015-05-31 22:03:35 +0100

    Fix to support django v1.7

--- django_enum_js/views.py
+++ django_enum_js/views.py
@@ -6,4 +6,4 @@ from django_enum_js import enum_wrapper
 
 def enums_js(request):
     enums = enum_wrapper.get_json_formatted_enums()
-    return render_to_response('django_enum_js/enums_js.tpl', { 'enums': mark_safe(enums), }, context_instance=RequestContext(request), mimetype='application/javascript')
+    return render_to_response('django_enum_js/enums_js.tpl', { 'enums': mark_safe(enums), }, context_instance=RequestContext(request), content_type='application/javascript')
